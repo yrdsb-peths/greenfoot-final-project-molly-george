4e3fc47b7fe75d4d57c4fb6a209544c2cd76e174
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2022-05-31 18:04:30 -0400

    rough titlescreen

--- Settings.java
+++ Settings.java
@@ -16,6 +16,6 @@ public class Settings extends World
     public Settings()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1280, 720, 1); 
     }
 }
