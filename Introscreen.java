4548760f0b74d68910c6dddce7fb557cf3022983
349225219 <101135430+349225219@users.noreply.github.com>
2022-06-01 11:33:26 -0400

    change

---
+++ Introscreen.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Introscreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Introscreen extends World
+{
+
+    /**
+     * Constructor for objects of class Introscreen.
+     * 
+     */
+    public Introscreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        
+        super(853, 480, 1); 
+        setBackground(new GreenfootImage("intropageee.jpg"));
+    }
+}
