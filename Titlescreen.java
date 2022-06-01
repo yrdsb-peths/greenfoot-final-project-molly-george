a2f5abc392a0f59677f33c18eeca0c23f11dc7d3
349225219 <101135430+349225219@users.noreply.github.com>
2022-05-31 21:41:03 -0400

    Revert "created title screen and transformation to intro screen"

--- Titlescreen.java
+++ Titlescreen.java
@@ -3,32 +3,25 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Titlescreen here.
  * 
- * @author Molly Wu 
- * @version May 2022
+ * @author (your name) 
+ * @version (a version number or a date)
  */
 public class Titlescreen extends World
 {
 
     /**
      * Constructor for objects of class Titlescreen.
-     * create a title screen and add a start label that leads to the next stage of the game
+     * 
      */
     public Titlescreen()
     {    
-        // Create a new world with 853x480 cells with a cell size of 1x1 pixels.
-        super(853, 480, 1); 
-        setBackground(new GreenfootImage("Titlescreenn.jpg"));
-
-        prepare();
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        setBackground(new GreenfootImage("pokemon titlescreenn.jpg"));
+        
     }
     
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the start label and add it to the world.
-     */
-    private void prepare()
+    public void act()
     {
-        StartLabel startLabel = new StartLabel();
-        addObject(startLabel, 200, 400);
     }
 }
