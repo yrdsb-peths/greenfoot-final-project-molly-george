c3624d953afea9fe3ba246e704120289892dc15a
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2022-06-01 21:42:41 -0400

    create welcome statement on introscreen

---
+++ Introi.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Introi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Introi extends Actor
+{
+    /**
+     * Act - do whatever the Introi wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        Introscreen i = (Introscreen) getWorld();
+        if(Greenfoot.mouseClicked(this))
+        {
+            i.removeObject(this);
+            
+        }
+    }
+}
