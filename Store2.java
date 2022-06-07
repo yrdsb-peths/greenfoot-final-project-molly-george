8570ca0e3f00b8e17de03b0f2923a3826eee94d8
349225219 <101135430+349225219@users.noreply.github.com>
2022-06-07 18:13:54 -0400

    change

--- Store2.java
+++ Store2.java
@@ -10,18 +10,35 @@ public class Store2 extends World
 {
     Label select = new Label("This two Pokemons will be the Pokemons you start with!",50);
     Label money = new Label("Now you have 50 coins to ",50);
-    
+    Bulbasaur b = new Bulbasaur();
+    Pikachu p = new Pikachu();
+    Charmander c = new Charmander();
+    UpGradeSecond1 s1 = new UpGradeSecond1();
+    UpGradeSecond2 s2 = new UpGradeSecond2();
+    UpGradeSecond3 s3 = new UpGradeSecond3();
     
     public Store2()
     {    
         super(1280, 720, 1); 
         
-        Bulbasaur b = new Bulbasaur();
-        //addObject
         
-        Pikachu p = new Pikachu();
-        //addObject
         
-        Charmander c = new Charmander();
     }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(b))
+        {
+            GeneralInformation.character[0] = "Bulbasaur";
+        }
+        if(Greenfoot.mouseClicked(p))
+        {
+            GeneralInformation.character[1] = "Bulbasaur";
+        }
+        
+    }
+    
+    
+    
+    
 }
---
+++ UpGradeSecond2.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class UpGradeSecond2 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UpGradeSecond2 extends NewLabel
+{
+    /**
+     * Act - do whatever the UpGradeSecond2 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
