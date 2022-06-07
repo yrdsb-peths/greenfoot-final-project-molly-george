8e9beb1cf9454f61809bf7a9640ece1113b998b9
349225219 <101135430+349225219@users.noreply.github.com>
2022-06-07 13:41:52 -0400

    some change

---
+++ ChangeMap.java
@@ -0,0 +1,25 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ChangeMap here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ChangeMap extends Actor
+{
+    public GreenfootImage[] faceRight = new GreenfootImage[9];
+    
+    public ChangeMap()
+    {
+        for(int i = 0; i < 9; i++)
+        {
+            //faceRight[i] = new GreenfootImage(); 
+        }
+    }
+    
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
--- FirstLevel.java
+++ FirstLevel.java
@@ -10,7 +10,11 @@ public class FirstLevel extends World
 {
     public int pikachuX = 650;
     public int pikachuY = 650;
-    
+    public int pikachuHP = 40;
+    public int enemy1HP = 60;
+    public int enemy2HP = 60;
+    public int enemy3HP = 60;
+    public Label nextMap = new Label("you passed this level, now click me get to the store!", 50); 
 
     /**
      * Constructor for objects of class FirstLevel.
@@ -18,10 +22,32 @@ public class FirstLevel extends World
      */
     public FirstLevel()
     {    
-        
         super(1280, 720, 1); 
+        
         
         Pikachu pikachu = new Pikachu();
         addObject(pikachu, pikachuX, pikachuY);
+    }
+    
+    public void act()
+    {
+        if(pikachuHP == 0)
+        {
+            //connect back to another world
+        }
+        if(enemy1HP == 0)
+        {
+            //send out the point to next map
+            
+        }
+        if(enemy1HP == 0 && enemy2HP == 0)
+        {
+              //send out point to next map  
+        }
+        if(enemy1HP == 0 && enemy2HP == 0 && enemy3HP == 0)
+        {
+              //send out you win   
+        }  
+        
     }
 }
