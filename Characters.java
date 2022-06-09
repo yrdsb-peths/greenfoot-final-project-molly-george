54d5d4c4a2b60f2bbbea48c78a39e5b6f504aa5e
349225219 <101135430+349225219@users.noreply.github.com>
2022-06-09 17:49:01 -0400

    change

--- Characters.java
+++ Characters.java
@@ -74,8 +74,5 @@ public class Characters extends Actor
     
     }
     
-    public void normalAttack()
-    {
-        
-    }
+    
 }
---
+++ NormalAttack.java
@@ -0,0 +1,31 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class NormalAttack here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NormalAttack extends Actor
+{
+    /**
+     * Act - do whatever the NormalAttack wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    public void normalAttack(int x)
+    {
+        setRotation(x);
+        move(4);
+        
+        
+        
+        
+        
+        
+    }
+}
