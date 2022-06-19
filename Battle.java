dee7e689f0b13c37f804cb04bc90f00b12eee942
349225219 <101135430+349225219@users.noreply.github.com>
2022-06-19 00:48:34 -0400

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -16,7 +16,7 @@ public class Battle extends World
     public Fennekin f = new Fennekin();
     public Torchic t = new Torchic();
     public Ninetales n = new Ninetales();
-    public static int  count = 2;
+    public static int  count = 1;
     public int bX = 0;
     public int bY;
     public int bZ;
@@ -33,6 +33,8 @@ public class Battle extends World
     public int hp2;
     public int hp3;
     public int hp4;
+    //chnage to array
+    private SimpleTimer[] timerArray = new SimpleTimer[11];
     private SimpleTimer timer = new SimpleTimer();
     private SimpleTimer timer1 = new SimpleTimer();
     private SimpleTimer timerRP = new SimpleTimer();
