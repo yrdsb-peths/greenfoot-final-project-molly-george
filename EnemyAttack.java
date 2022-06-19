472a2f9abb2055d35d4e56e0415d3c6af6ba34b0
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2022-06-19 01:38:39 -0400

    general comments

--- EnemyAttack.java
+++ EnemyAttack.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class EnemyAttack here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author George Lu && Molly Wu 
+ * @version June 2022
  */
 public class EnemyAttack extends Damage
 {
