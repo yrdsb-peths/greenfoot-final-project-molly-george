1aae5fd441fbf6ce4dd43e8818e8ba4933712484
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2022-06-18 20:06:24 -0400

    Update Load1.java

--- Load1.java
+++ Load1.java
@@ -15,7 +15,6 @@ public class Load1 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(900, 506, 1); 
-        addObject(g,450,250);
         
         setBackground(new GreenfootImage("transitionlevel1.jpg"));
         g.clicki = false;
