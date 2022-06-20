8f7b9e517086e346db5bc60df7fc4ad70a457e90
Mollyyy7 <101139130+Mollyyy7@users.noreply.github.com>
2022-06-19 20:35:29 -0400

    add instructions

--- IntroScreen.java
+++ IntroScreen.java
@@ -37,8 +37,8 @@ public class IntroScreen extends World
             dialogue.ifNextButtonClicked = true;
             if(dialogue.idx == 5)
             {
-                Map1 m1 = new Map1();
-                Greenfoot.setWorld(m1);
+                Instructions instructions = new Instructions();
+                Greenfoot.setWorld(instructions);
             }
         }
         
